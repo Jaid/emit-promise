d5a2ae58e56bb53080c3101f516db5e5f7bb673e
Jaid <jaid.jsx@gmail.com>
2019-05-29 01:57:04 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 /** @module emit-promise */
 
 /**
- * Returns the number of seconds passed since Unix epoch (01 January 1970)
+ * Emits an event to the socket and resolves the socket's answer in a Promise
  * @example
  * import emitPromise from "emit-promise"
- * emitPromise()
- * // 1549410770
- * @returns {number} Seconds since epoch
+ * const status = await emitPromise(socketIoServerSocket, "getStatus")
+ * @param {object} socket Socket object with an `emit` function
+ * @param {...payload} payload Things to send with the event
+ * @returns {Promise<any>} Answer from socket
  */
-export default () => Math.floor(Date.now() / 1000)
+export default (socket, ...payload) => new Promise((resolve, reject) => {
+  try {
+    socket.emit(...payload, response => {
+      resolve(response)
+    })
+  } catch (error) {
+    reject(error)
+  }
+})
--- test/test.js
+++ test/test.js
@@ -3,7 +3,4 @@ import path from "path"
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 const {default: emitPromise} = indexModule
 
-it("should run", () => {
-  const result = emitPromise()
-  expect(result).toBeGreaterThan(1549410770)
-})
+test.todo("Do it later!")
