edff776a62fbf7b2b98c66ecbb1f2843e050a4c5
Jaid <jaid.jsx@gmail.com>
2019-07-26 11:34:18 +0200

    feat: Added export `emitTimeout`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /** @module emit-promise */
 
+import pTimeout from "p-timeout"
+
 /**
  * Emits an event to the socket and resolves the socket's answer in a Promise
  * @example
@@ -9,7 +11,7 @@
  * @param {...payload} payload Things to send with the event
  * @returns {Promise<any>} Answer from socket
  */
-export default (socket, ...payload) => new Promise((resolve, reject) => {
+const emitPromise = (socket, ...payload) => new Promise((resolve, reject) => {
   try {
     socket.emit(...payload, response => {
       resolve(response)
@@ -18,3 +20,21 @@ export default (socket, ...payload) => new Promise((resolve, reject) => {
     reject(error)
   }
 })
+
+/**
+ * Emits an event to the socket and resolves the socket's answer in a Promise
+ * @async
+ * @function
+ * @param {object} socket Socket object with an `emit` function
+ * @param {number} timeout Timeout in milliseconds
+ * @param {...payload} payload Things to send with the event
+ * @returns {Promise<any>} Answer from socket
+ * @example
+ * import emitPromise from "emit-promise"
+ * const status = await emitPromise.withTimeout(socketIoServerSocket, 15000, "getStatus")
+ */
+export const emitTimeout = async (socket, timeout, ...payload) => {
+  return pTimeout(emitPromise(socket, ...payload), timeout)
+}
+
+export default emitPromise
