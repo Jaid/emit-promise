51c316c4c4a268948b67da58c95ad10b64394ea9
Jaid <jaid.jsx@gmail.com>
2019-07-27 17:02:34 +0200

    feat: Removed emitPromise.withTimeout and made timeout value required
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 /** @module emit-promise */
 
 import pTimeout from "p-timeout"
+import ms from "ms.macro"
 
-/**
- * Emits an event to the socket and resolves the socket's answer in a Promise
- * @example
- * import emitPromise from "emit-promise"
- * const status = await emitPromise(socketIoServerSocket, "getStatus")
- * @param {object} socket Socket object with an `emit` function
- * @param {...payload} payload Things to send with the event
- * @returns {Promise<any>} Answer from socket
- */
 const emitPromise = (socket, ...payload) => new Promise((resolve, reject) => {
   try {
     socket.emit(...payload, response => {
@@ -25,16 +17,19 @@ const emitPromise = (socket, ...payload) => new Promise((resolve, reject) => {
  * Emits an event to the socket and resolves the socket's answer in a Promise
  * @async
  * @function
- * @param {object} socket Socket object with an `emit` function
- * @param {number} timeout Timeout in milliseconds
- * @param {...payload} payload Things to send with the event
+ * @param {{emit: (eventName: string, payload: ...*, callback: * => void) => void}} socket Socket object with an `emit` function
+ * @param {number|boolean} timeout Timeout in milliseconds, `true` for default value of 60 seconds, `false` to disable timeout and risk waiting forever
+ * @param {...*} payload Things to send with the event
  * @returns {Promise<any>} Answer from socket
  * @example
  * import emitPromise from "emit-promise"
- * const status = await emitPromise.withTimeout(socketIoServerSocket, 15000, "getStatus")
+ * const status = await emitPromise(socketIoServerSocket, 15000, "getStatus")
  */
-export const emitTimeout = async (socket, timeout, ...payload) => {
-  return pTimeout(emitPromise(socket, ...payload), timeout)
+export default async (socket, timeout, ...payload) => {
+  const promise = emitPromise(socket, ...payload)
+  if (timeout === false) {
+    return promise
+  }
+  const timeoutMs = timeout === true ? ms`60 seconds` : timeout
+  return pTimeout(promise, timeoutMs)
 }
-
-export default emitPromise
