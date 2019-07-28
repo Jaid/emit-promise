ff9c6949644414993c707c1a9f29c29bd6f9537b
Jaid <jaid.jsx@gmail.com>
2019-07-28 12:58:41 +0200

    feat: Added members `withDefaultTimeout` and `withoutTimeout`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const emitPromise = (socket, ...payload) => new Promise((resolve, reject) => {
  * import emitPromise from "emit-promise"
  * const status = await emitPromise(socketIoServerSocket, 15000, "getStatus")
  */
-export default async (socket, timeout, ...payload) => {
+const main = (socket, timeout, ...payload) => {
   const promise = emitPromise(socket, ...payload)
   if (timeout === false) {
     return promise
@@ -33,3 +33,35 @@ export default async (socket, timeout, ...payload) => {
   const timeoutMs = timeout === true ? ms`60 seconds` : timeout
   return pTimeout(promise, timeoutMs)
 }
+
+/**
+ * Emits an event to the socket and resolves the socket's answer in a Promise with a timeout of 60 seconds
+ * @async
+ * @function
+ * @param {{emit: (eventName: string, payload: ...*, callback: * => void) => void}} socket Socket object with an `emit` function
+ * @param {...*} payload Things to send with the event
+ * @returns {Promise<any>} Answer from socket
+ * @example
+ * import emitPromise from "emit-promise"
+ * const status = await emitPromise.withDefaultTimeout(socketIoServerSocket, "getStatus")
+ */
+main.withDefaultTimeout = async (socket, ...payload) => {
+  return main(socket, true, ...payload)
+}
+
+/**
+ * Emits an event to the socket and resolves the socket's answer in a Promise, never timeouts (possible dangerous)
+ * @async
+ * @function
+ * @param {{emit: (eventName: string, payload: ...*, callback: * => void) => void}} socket Socket object with an `emit` function
+ * @param {...*} payload Things to send with the event
+ * @returns {Promise<any>} Answer from socket
+ * @example
+ * import emitPromise from "emit-promise"
+ * const status = await emitPromise.withoutTimeout(socketIoServerSocket, "getStatus")
+ */
+main.withoutTimeout = async (socket, ...payload) => {
+  return main(socket, false, ...payload)
+}
+
+export default main
